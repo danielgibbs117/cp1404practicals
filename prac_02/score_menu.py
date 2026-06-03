6e171e2d9d824a537a03e83ea1672a30194b6b79
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-06-03 23:10:28 +1000

    copied calculate score from previous file "score.py"

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -25,3 +25,14 @@ def get_valid_score():
         score = int(input("Score: "))
 
     return score
+
+
+def calculate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
