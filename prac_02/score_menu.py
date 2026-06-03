c55cfa968838d8ea3cbbcd328da60a26169c65c5
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-06-03 22:15:16 +1000

    Add pseudocode

---
+++ prac_02/score_menu.py
@@ -0,0 +1,19 @@
+"""
+print menu
+get choice
+while choice != <quit option>
+    if choice == <first option>
+        <do first task>
+    else if choice == <second option>
+        <do second task>
+    ...
+    else if choice == <n-th option>
+        <do n-th task>
+    else
+        print invalid input error message
+    print menu
+    get choice
+<do final thing, if needed>
+"""
+
+
