cb25cdd60fc4262d7e84b681de162990246854f4
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-06-10 21:14:07 +1000

    Add TODO for f-string and for loop

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -34,6 +34,7 @@ for i, number in enumerate(numbers, 1):
 
 # TODO: Use f-string formatting to produce the output:
 # 1922 Gibson L-5 CES for about $16,036!
+print("{} {} for about ${:,.0f}!".format(year, name, cost))
 
 # TODO: Using a for loop with the range function and f-string formatting,
 # produce the following right-aligned output (DO NOT use a list):
@@ -48,3 +49,7 @@ for i, number in enumerate(numbers, 1):
 # 2 ^ 8 is  256
 # 2 ^ 9 is  512
 # 2 ^10 is 1024
+
+for i in range(11):
+    number = 2 ** i
+    print(f"2 ^{i:>2} is {number:>4}")
