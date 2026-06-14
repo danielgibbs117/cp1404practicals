5d72dae70385d0905fee25eab91392551d5712f7
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-06-14 12:58:17 +1000

    Add "Change the line that prints the report so that it uses an f-string instead of the .format method."

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -18,7 +18,7 @@ def main():
     for month in range(1, months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
