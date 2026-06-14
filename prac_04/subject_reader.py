04d8e237d0ccb22d157957c8325eb46ae8fe97b6
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-06-14 20:19:17 +1000

    Add display_subject_details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,12 +9,14 @@ FILENAME = "subject_data.txt"
 def main():
     """Program to load and display subject data from file."""
     data = load_data(FILENAME)
+    display_subject_details(data)
     print(data)
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    subject_data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,9 +25,19 @@ def load_data(filename=FILENAME):
         print(parts)  # See what the parts look like (notice the integer is a string)
         # Make the number an integer as part of a new, poorly named, list
         data = [parts[0], parts[1], int(parts[2])]
+        subject_data.append(data)
         print(data)  # See if that worked
         print("----------")
     input_file.close()
+    return subject_data
+
+
+def display_subject_details(subject_data):
+    for subject in subject_data:
+        subject_code = subject[0]
+        lecturer = subject[1]
+        students = subject[2]
+        print(f"{subject_code} is taught by {lecturer} and has {students} students")
 
 
 main()
