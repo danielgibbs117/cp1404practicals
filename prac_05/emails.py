1cc6d78c126776e4aaefb13c0e6b63682917d528
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-06-23 23:01:34 +1000

    add emails.py

---
+++ prac_05/emails.py
@@ -0,0 +1,34 @@
+"""
+Word Occurrences
+Estimate:  minutes
+Actual:    minutes
+"""
+
+
+def extract_name(email):
+    username = email.split("@")[0]
+    name_parts = username.split(".")
+    name = " ".join(name_parts).title()
+    return name
+
+
+email_to_name = {}
+
+email = input("Email: ")
+
+while email != "":
+    name = extract_name(email)
+
+    confirmation = input(f"Is your name {name}? (Y/n) ")
+
+    if confirmation.lower() not in ("", "y"):
+        name = input("Name: ")
+
+    email_to_name[email] = name
+
+    email = input("Email: ")
+
+print()
+
+for email, name in email_to_name.items():
+    print(f"{name} ({email})")
