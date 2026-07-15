ce7a8ba5a130ffde8127037ee4fa4fa8c41e0115
Daniel <daniel.gibbs@my.jcu.edu.au>
2026-07-16 00:02:52 +1000

    add str of car in car.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -12,6 +12,10 @@ class Car:
         self.fuel = fuel
         self._odometer = 0
 
+    def __str__(self):
+        """Return string of car"""
+        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
+
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
         self.fuel += amount
@@ -29,4 +33,3 @@ class Car:
             self.fuel -= distance
         self._odometer += distance
         return distance
-    
